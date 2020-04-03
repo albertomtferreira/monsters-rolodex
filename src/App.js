21ea29a36e6562c49a90ad6311fb0150eafd35e0
albertomtferreira <60177437+albertomtferreira@users.noreply.github.com>
2020-04-03 23:42:14 +0100

    upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      data: []
+    };
+  }
+  componentDidMount(){
+    fetch('https://corona.lmao.ninja/countries?sort=country')
+      .then(response => response.json())
+      .then(country => this.setState({data: country}))
+      // .then(countryCases => this.setState({data: countryCases}))
+  }
+
+  
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.data.map(country => (
+          <div key={country.countryInfo.iso3} >
+          <img alt={`${country.countryInfo.iso3}`} src={country.countryInfo.flag}/>
+          <h1 >Country: {country.country}</h1>
+          <h2 >Total Cases:{country.cases}</h2>
+          <h2 >Total Deaths: {country.deaths}</h2>
+          <h3 >Total Active: {country.active}</h3>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
+
